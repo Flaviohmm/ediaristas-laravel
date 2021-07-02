28ea8156924832dc357eb74db84f8889158d163f
Flaviohmm <fhenrique609@gmail.com>
2021-07-02 20:28:32 -0300

    Conectando API ao Front-End

--- app/Models/Diarista.php
+++ app/Models/Diarista.php
@@ -9,15 +9,62 @@ class Diarista extends Model
 {
     use HasFactory;
 
+    /**
+     * Defini os campos que podem ser gravados
+     *
+     * @var array
+     */
     protected $fillable = ['nome_completo', 'cpf', 'email', 'telefone', 'logradouro', 'numero', 'complemento', 'bairro', 'cidade', 'estado', 'cep', 'codigo_ibge', 'foto_usuario'];
 
-    protected $visible = ['nome_completo', 'cidade', 'foto_usuario'];
+    /**
+     * Define os campos que serão usados na serialização
+     *
+     * @var array
+     */
+    protected $visible = ['nome_completo', 'cidade', 'foto_usuario', 'reputacao'];
 
+     /**
+     * Adiciona campos na serialização
+     *
+     * @var array
+     */
+    protected $appends = ['reputacao'];
+
+    /**
+     * Monta a URL da imagem
+     *
+     * @param string $valor
+     */
+    public function getFotoUsuarioAttribute(string $valor)
+    {
+        return config('app.url') . '/' . $valor;
+    }
+
+    /**
+     * Retorna a reputação randomica
+     *
+     * @param [type] $valor
+     */
+    public function getReputacaoAttribute($valor)
+    {
+        return mt_rand(1, 5);
+    }
+
+    /**
+     * Busca diaristas por código IBGE
+     *
+     * @param integer $codigoIbge
+     */
     static public function buscaPorCodigoIbge(int $codigoIbge)
     {
         return self::where('codigo_ibge', $codigoIbge)->limit(6)->get();
     }
 
+    /**
+     * Retorna a quantidade de diaristas
+     *
+     * @param integer $codigoIbge
+     */
     static public function quantidadePorCodigoIbge(int $codigoIbge)
     {
         $quantidade = self::where('codigo_ibge', $codigoIbge)->count();
